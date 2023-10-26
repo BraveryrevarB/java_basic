7233b0b794d214410487e0bfff088829fff2a5b9
chentianxin <15958307871@163.com>
2023-10-26 09:20:03 +0800

    测试一下分支

---
+++ src/type_conversion.java
@@ -0,0 +1,8 @@
+public class type_conversion {
+    public static void main(String [] args){
+        byte a = 30;
+        byte b = 20;
+        int c = a * b;
+        System.out.println(c);
+    }
+}
